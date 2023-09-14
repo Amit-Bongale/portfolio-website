c868c5162946c511d3e081d37c3c49c72266dce2
Amit-Bongale <115764402+Amit-Bongale@users.noreply.github.com>
2023-09-14 17:34:32 +0530

    nav fixed

--- js/nav.js
+++ js/nav.js
@@ -1,36 +1,26 @@
-// const btn = document.querySelector("#checkbox2");
-// const menu = document.querySelector(".Menu")
-
-// btn.addEventListener("onclick" , () => {
-//     var element = document.getElementById("nav-toggle");
-
-//     element.classList.remove("visility-false");
-//     element.classList.add("visiblity-true")
-
-//     console.log("tiggered")
-// })
-
 
 let i = 1;
+
 function myfunction(){
     
     var element = document.getElementById("nav-toggle");
-    head = document.querySelector(".head")
+    var head = document.querySelector(".head");
 
-    if (i == 0){
+    if (i == 0) {
         element.classList.add("visility-false");
         element.classList.remove("visility-true");
         i = 1;
-        console.log("nav hidden")
         head.style.overflowX = "hidden";
+        // console.log("nav hidden")
 
     }
 
     else {
         element.classList.remove("visility-false");
+        head.classList.remove("overflow");
         element.classList.add("visility-true");
         i = 0;
-        console.log("nav visible")
         head.style.overflowX = "visible";
+        // console.log("nav visible")
     }
 }
