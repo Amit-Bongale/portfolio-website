4528178acf59e39b77e6627dbca6e69939e7b82e
Amit-Bongale <115764402+Amit-Bongale@users.noreply.github.com>
2023-10-22 00:24:56 +0530

    overflow fixed

--- js/nav.js
+++ js/nav.js
@@ -1,16 +1,19 @@
 
+
 let i = 1;
 
 function myfunction(){
     
     var element = document.getElementById("nav-toggle");
     var head = document.querySelector(".head");
+    const body = document.body;
 
     if (i == 0) {
         element.classList.add("visility-false");
         element.classList.remove("visility-true");
         i = 1;
         head.style.overflowX = "hidden";
+        body.style.overflow = "auto";
         // console.log("nav hidden")
 
     }
@@ -21,6 +24,8 @@ function myfunction(){
         element.classList.add("visility-true");
         i = 0;
         head.style.overflowX = "visible";
+        body.style.overflow = "hidden";
         // console.log("nav visible")
     }
 }
+
